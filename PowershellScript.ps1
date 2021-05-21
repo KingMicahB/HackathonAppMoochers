26e19431c6889bf722ab9bc43bf768eff00061e5
tzvi <timothy.vogel@mail.huji.ac.il>
2021-05-21 18:53:58 +0300

    pushed powershell script

---
+++ PowershellScript.ps1
@@ -0,0 +1,28 @@
+﻿cd C:\Users\tzvi\Desktop\Huji_Hackathon\AnimalFriendConnector
+
+$seconds_wait = 0
+
+
+while($true){
+
+    $size_var = Get-Content -Path C:\Users\tzvi\Desktop\Huji_Hackathon\AnimalFriendConnector\resource.txt
+
+
+    if($seconds_wait -ne $size_var[0])
+    {
+        $seconds_wait = $size_var[0]
+    }
+
+
+    Start-Sleep -s $seconds_wait
+
+
+    Get-Process | Where-Object { $_.MainWindowTitle} | Where-Object { $_.ProcessName -ne "powershell_ise"} | Where-Object { $_.ProcessName -ne "zoom"} | kill 
+    Start-Process java '-jar C:\Users\tzvi\Desktop\Huji_Hackathon\AnimalFriendConnector\AnimalFriendConnector.jar'
+    $p = Get-Process java
+    Wait-Process -Id $p.id
+
+}
+    
+
+
